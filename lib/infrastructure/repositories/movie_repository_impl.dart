9469680283fb79b65998c196c6d28cd1c748ad7b
Gastón <gaston-gaitan@hotmail.com>
2025-02-09 13:43:32 -0300

    commit

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,15 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+
+class MovieRepositoryImpl extends MoviesRepository{
+
+  final MoviedbDatasource datasource;
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+
+  }
+}
